28c53c9e4fd6f5ad8aac3b660f1cdf3b9fce53d7
Davy Josias Scheuermann <davy.josias@gmail.com>
2025-08-01 10:57:16 -0300

    Configuração do routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :clients do
+        resources :addresses, only: [:index]
+      end
+    end
+  end
 end
