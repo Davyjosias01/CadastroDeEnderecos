4ac17453aec28b57f859c2fdfc009054fd742bd1
Davy Josias Scheuermann <davy.josias@gmail.com>
2025-08-05 10:15:25 -0300

    Ajustes nos controllers da api, do clients, index do clients application.html e assets do initializers

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::ClientsController < ApplicationController
+  def index
+    client = Client.find(params[:client_id]) unless params[:client_id].blank?
+    render json: client
+  rescue StandartError => e
+    invalid_attempt e.message
+  end
+
+  def invalid_attempt(reason, status = :unprocessable_entity)
+    render json:{
+      success: false,
+      error: reason
+    }, status: status
+  end
 end
